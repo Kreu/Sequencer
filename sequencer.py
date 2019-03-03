dfc70b8ed7ef946fc5f8d98e66ea9bd8e612cd74
Kreu <kruusvee.valdeko@gmail.com>
2019-03-03 12:26:56 +0000

    fixed indentation

--- sequencer.py
+++ sequencer.py
@@ -16,22 +16,22 @@ logging.basicConfig(level=logging.INFO,
 parser = argparse.ArgumentParser(description='Process sequencing files')
 
 parser.add_argument('-n',
-										metavar='nucleotides', 
-										help='Input file in FASTA format containing the nucleotide sequence')
+                    metavar='nucleotides', 
+                    help='Input file in FASTA format containing the nucleotide sequence')
 parser.add_argument('-s',
-										metavar='scores',
-										help='Input file in FASTA format containing the nucleotide scores')
+                    metavar='scores',
+                    help='Input file in FASTA format containing the nucleotide scores')
 parser.add_argument('-f',
-										metavar='signal_cutoff',
-										type=int,
-										default=20,
-										help='Signal cutoff value under which a base call is rejected. Default value is 20')
+                    metavar='signal_cutoff',
+                    type=int,
+                    default=20,
+                    help='Signal cutoff value under which a base call is rejected. Default value is 20')
 parser.add_argument('-o',
-										metavar='output',
-										help='Output file for the results')
+                    metavar='output',
+                    help='Output file for the results')
 parser.add_argument('-r', 
-										help='Specifies that the sequencing file needs to be reverse complemented',
-										action='store_true')
+                    help='Specifies that the sequencing file needs to be reverse complemented',
+                    action='store_true')
 
 arguments = parser.parse_args()
 
@@ -40,17 +40,17 @@ arguments = parser.parse_args()
 #Reads a FASTA file containing a single sequence and returns the sequence information
 #If the file contains more than one sequence, the extra sequences are not processed
 def ReadFile(filename):
-	with open(filename, 'r') as file:
-		header_found = False;
-		for line in file:
-			#Desired content is always on the next line after the FASTA header, which 
-			#itself always starts with a '>' character.
-			if line.startswith('>'):
-				header_found = True
-				continue
+  with open(filename, 'r') as file:
+    header_found = False;
+    for line in file:
+      #Desired content is always on the next line after the FASTA header, which 
+      #itself always starts with a '>' character.
+      if line.startswith('>'):
+        header_found = True
+        continue
 
-			if header_found:
-				return line.strip('\n')
+      if header_found:
+        return line.strip('\n')
 
 
 #Filters nucleotides based on the signal cutoff.
@@ -60,102 +60,102 @@ def ReadFile(filename):
 #whose score is lower than the cutoff value with a '-' (dash)
 #
 #Parameters:
-#		nucleotide_list = A list of nucleotides
-#		scores_list = A list of nucleotide scores that correspond to the nucleotides in 
-#									nucleotide_list
-#		quality_cutoff = -10*log10(P) value where P is the probability of a miscall.
-#						quencer
-#				For example, a signal cutoff value of 30 means there is a 
-#										1/1000 chance of miscalled base. (log10(1/1000) = -3 * -10 = 30)
+#   nucleotide_list = A list of nucleotides
+#   scores_list = A list of nucleotide scores that correspond to the nucleotides in 
+#                 nucleotide_list
+#   quality_cutoff = -10*log10(P) value where P is the probability of a miscall.
+#           quencer
+#       For example, a signal cutoff value of 30 means there is a 
+#                   1/1000 chance of miscalled base. (log10(1/1000) = -3 * -10 = 30)
 #
 #Returns:
-#		A list of filtered nucleotides with a score equal to or above the cutoff value.
-#		All base calls below the threshold are replaced by gaps ('-').
+#   A list of filtered nucleotides with a score equal to or above the cutoff value.
+#   All base calls below the threshold are replaced by gaps ('-').
 #
 #EXAMPLE
 #TODO
 def FilterSequence(nucleotide_list, quality_list, quality_cutoff):
-	logging.debug(f'Input nucleotides list: {nucleotide_list}')
-	logging.debug(f'Input quality scores list: {quality_list}')
+  logging.debug(f'Input nucleotides list: {nucleotide_list}')
+  logging.debug(f'Input quality scores list: {quality_list}')
 
-	#Check whether either of the lists are empty (could indicate bad sequencing file)
-	if not nucleotide_list:
-		logging.error('Nucletide sequence list is empty, aborting')
-		return
+  #Check whether either of the lists are empty (could indicate bad sequencing file)
+  if not nucleotide_list:
+    logging.error('Nucletide sequence list is empty, aborting')
+    return
 
-	if not quality_list:
-		logging.error('Quality scores list is empty, aborting')
-		return
+  if not quality_list:
+    logging.error('Quality scores list is empty, aborting')
+    return
 
-	logging.debug(f'Nucleotide list length: {len(nucleotide_list)}, quality list length: {len(quality_list)}')
-	if len(nucleotide_list) != len(quality_list):
-		raise ValueError('Nucleotide and scores lists are not equal in length, indicating malformed data')
-
-
-	logging.info("Signal threshold has been set to " + str(quality_cutoff))
-	position = 0
-	filtered_sequence = []
-
-	for quality_score in quality_list:
-		if int(quality_score) >= quality_cutoff:
-			filtered_sequence.append(nucleotide_list[position])
-
-		if int(quality_score) < quality_cutoff:
-			filtered_sequence.append('-')
-
-		position += 1
+  logging.debug(f'Nucleotide list length: {len(nucleotide_list)}, quality list length: {len(quality_list)}')
+  if len(nucleotide_list) != len(quality_list):
+    raise ValueError('Nucleotide and scores lists are not equal in length, indicating malformed data')
 
 
-	return filtered_sequence
+  logging.info("Signal threshold has been set to " + str(quality_cutoff))
+  position = 0
+  filtered_sequence = []
+
+  for quality_score in quality_list:
+    if int(quality_score) >= quality_cutoff:
+      filtered_sequence.append(nucleotide_list[position])
+
+    if int(quality_score) < quality_cutoff:
+      filtered_sequence.append('-')
+
+    position += 1
+
+
+  return filtered_sequence
 
 
 def WriteOutput(output_file, data):
-	with open(output_file, 'w') as output:
-		output.write(data)
-	print("Writing results into " + output_file)
+  with open(output_file, 'w') as output:
+    output.write(data)
+  print("Writing results into " + output_file)
 
 
 def ReverseComplement(sequence):
-	reverse_sequence = sequence[::-1]
-	reverse_complement = ""
-	for c in reverse_sequence:
-		if c == 'A': reverse_complement += 'T'
-		elif c == 'a': reverse_complement += 't'
-		elif c == 'G': reverse_complement += 'C'
-		elif c == 'g': reverse_complement += 'c'
-		elif c == 'C': reverse_complement += 'G'
-		elif c == 'c': reverse_complement += 'g'
-		elif c == 'T': reverse_complement += 'A'
-		elif c == 't': reverse_complement += 'a'
-		else: reverse_complement += c
+  reverse_sequence = sequence[::-1]
+  reverse_complement = ""
+  for c in reverse_sequence:
+    if c == 'A': reverse_complement += 'T'
+    elif c == 'a': reverse_complement += 't'
+    elif c == 'G': reverse_complement += 'C'
+    elif c == 'g': reverse_complement += 'c'
+    elif c == 'C': reverse_complement += 'G'
+    elif c == 'c': reverse_complement += 'g'
+    elif c == 'T': reverse_complement += 'A'
+    elif c == 't': reverse_complement += 'a'
+    else: reverse_complement += c
 
-	return reverse_complement
+  return reverse_complement
 
 
 def main():
-	logging.info(f'Nucleotide sequence file: {arguments.n}')
-	logging.info(f'Nucleotide scores file: {arguments.s}')
+  logging.info(f'Nucleotide sequence file: {arguments.n}')
+  logging.info(f'Nucleotide scores file: {arguments.s}')
 
-	#Nucleotide quality scores comes as a string of space-separated numbers so we need 
-	#to split them by space to get a list of individual scores.
-	nucleotide_scores = ReadFile(arguments.s).split(' ')
-	nucleotide_sequence = ReadFile(arguments.n)
+  #Nucleotide quality scores comes as a string of space-separated numbers so we need 
+  #to split them by space to get a list of individual scores.
+  nucleotide_scores = ReadFile(arguments.s).split(' ')
+  nucleotide_sequence = ReadFile(arguments.n)
 
-	if (arguments.r):
-		logging.info("Reverse complement specified")
+  if (arguments.r):
+    logging.info("Reverse complement specified")
 
-		nucleotide_scores = nucleotide_scores[::-1]
+    nucleotide_scores = nucleotide_scores[::-1]
 
-		nucleotide_sequence = ReverseComplement(nucleotide_sequence)
+    nucleotide_sequence = ReverseComplement(nucleotide_sequence)
 
-	nucleotide_sequence = list(nucleotide_sequence)
+  nucleotide_sequence = list(nucleotide_sequence)
 
-	signal_cutoff = arguments.f
-	filtered_sequence = FilterSequence(nucleotide_sequence, nucleotide_scores, signal_cutoff)
+  signal_cutoff = arguments.f
+  filtered_sequence = FilterSequence(nucleotide_sequence, nucleotide_scores, signal_cutoff)
 
-	output_file = arguments.o
-	nucleotide_data = ''.join(filtered_sequence)
-	WriteOutput(output_file, nucleotide_data)
+  output_file = arguments.o
+  nucleotide_data = ''.join(filtered_sequence)
+  WriteOutput(output_file, nucleotide_data)
 
 if __name__ == '__main__':
-	main()
+  main()
